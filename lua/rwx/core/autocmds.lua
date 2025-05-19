9da5761579b16e2dab97b9511a1dfd9402691273
rxf <rxf431@gmail.com>
2025-05-19 07:26:15 -0300

    2025-05-19T07:26:15

--- lua/rwx/core/autocmds.lua
+++ lua/rwx/core/autocmds.lua
@@ -67,21 +67,3 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdLineEn
 		end
 	end,
 })
-
--- LSP
--- local completion = vim.g.completion_mode or "blink"
--- vim.api.nvim_create_autocmd("LspAttach", {
--- 	callback = function(args)
--- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
--- 		if client then
--- 			-- Built-in completion
--- 			if completion == "native" and client:supports_method("textDocument/completion") then
--- 				vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
--- 			end
--- 			-- Inlay hints
--- 			if client:supports_method("textDocument/inlayHints") then
--- 				vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
--- 			end
--- 		end
--- 	end,
--- })
